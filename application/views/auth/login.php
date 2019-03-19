d15a8cf89e28b92ea90824331a1bfb7148a20083
pavlosb <pavlos.bizimis@gmail.com>
2019-03-19 19:01:01 +0200

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,28 +3,29 @@
   <div class="col-sm-8 col-lg-6">
 <h1 class="display-4"><?php echo lang('login_heading');?></h1>
 <p class="lead"><?php echo lang('login_subheading');?></p>
-
+<?php if ($message !=""){ ?>
 <div class="alert alert-danger" role="alert"><?php echo $message;?></div>
+<?php } ?>
 </div>
 </div>
 <div class="row justify-content-center">
 <div class="col-sm-8 col-lg-6">
 <?php echo form_open("auth/login");?>
 
-  <p>
+<div class="form-group">
   <label for="identity"><?php echo lang('login_identity_label', 'identity');?></label>
     <?php echo form_input($identity);?>
-  </p>
+    </div>
 
-  <p>
+  <div class="form-group">
   <label for="password"><?php echo lang('login_password_label', 'password');?></label>
     <?php echo form_input($password);?>
-  </p>
+    </div>
 
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
+  <div class="form-group form-check">
     <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+    <label class="form-check-label" for="remember"><?php echo lang('login_remember_label', 'remember');?></label>
+  </div>
 
 
   <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
