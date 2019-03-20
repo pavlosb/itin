f4b56b89bfce045753f1a18a50820b96138f84c2
pavlosb <pavlos.bizimis@gmail.com>
2019-03-20 14:39:41 +0200

    Update index.php

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -16,27 +16,25 @@
 <table class= "table table-striped">
 <thead>
 	<tr>
-		<th scope="col"><?php echo lang('index_fname_th');?></th>
-		<th scope="col"><?php echo lang('index_lname_th');?></th>
+		<th scope="col"><?php echo lang('index_fname_th');?> <?php echo lang('index_lname_th');?></th>
 		<th scope="col"><?php echo lang('index_email_th');?></th>
-		<th scope="col"><?php echo lang('index_groups_th');?></th>
-		<th scope="col"><?php echo lang('index_status_th');?></th>
-		<th scope="col"><?php echo lang('index_action_th');?></th>
+		<th scope="col" class="text-center"><?php echo lang('index_groups_th');?></th>
+		<th scope="col" class="text-center"><?php echo lang('index_status_th');?></th>
+		<th scope="col" class="text-center"><?php echo lang('index_action_th');?></th>
 	</tr>
 	</thead>
   <tbody>
 	<?php foreach ($users as $user):?>
 		<tr>
-            <td><?php echo htmlspecialchars($user->first_name,ENT_QUOTES,'UTF-8');?></td>
-            <td><?php echo htmlspecialchars($user->last_name,ENT_QUOTES,'UTF-8');?></td>
+            <td><?php echo htmlspecialchars($user->first_name,ENT_QUOTES,'UTF-8');?> <?php echo htmlspecialchars($user->last_name,ENT_QUOTES,'UTF-8');?></td>
             <td><?php echo htmlspecialchars($user->email,ENT_QUOTES,'UTF-8');?></td>
-			<td>
+			<td class="text-center">
 				<?php foreach ($user->groups as $group):?>
 					<?php echo anchor("auth/edit_group/".$group->id, htmlspecialchars($group->name,ENT_QUOTES,'UTF-8')) ;?><br />
                 <?php endforeach?>
 			</td>
 			<td class="text-center"><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, '<i class="far fa-traffic-light-go fa-2x"></i>') : anchor("auth/activate/". $user->id, '<i class="fal fa-traffic-light-stop fa-2x"></i>');?></td>
-			<td><?php echo anchor("auth/edit_user/".$user->id, 'Edit') ;?></td>
+			<td class="text-center"><?php echo anchor("auth/edit_user/".$user->id, 'Edit') ;?></td>
 		</tr>
 	<?php endforeach;?>
 	</tbody>
