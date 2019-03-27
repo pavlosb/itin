1e9605bcb0735d13dc33b5a1f9c476b5bbcb58ab
pavlosb <pavlos.bizimis@gmail.com>
2019-03-27 19:28:31 +0200

    Update checkpoints.php

--- application/views/checkpoints.php
+++ application/views/checkpoints.php
@@ -1,18 +1,22 @@
 <div class="container">
 <div class="row justify-content-center">
   <div class="col-sm-8 col-lg-6">
-<?php print_r($checkpoints); ?>
+
 <table>
 <?php 
 $mcp = "";
+$scp = "";
 foreach ($checkpoints as $cp): 
 if ($cp['mainsect'] != $mcp) { ?>
 <tr><td colspan="3"><?= $cp['mainsect']; ?></td></tr>
+<?php } 
+if ($cp['name_section'] != $scp) { ?>
+<tr><td colspan="3"><?= $cp['name_section']; ?></td></tr>
 <?php } ?>
-<tr><td><?= $cp['name_section']; ?></td><td><?= $cp['name_cp']; ?></td><td><?= $cp['points_cp']; ?></td></tr>
+<tr><td><?= $cp['name_cp']; ?></td><td><?= $cp['points_cp']; ?></td><td></td></tr>
 <?php
 $mcp = $cp['mainsect'];
-
+$smcp = $cp['name_section'];
  endforeach ?>
 </table>
 </div>
