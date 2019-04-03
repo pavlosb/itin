5b5e473fbb2326a5ae7ea2c4b9a9651fd4397186
pavlosb <pavlos.bizimis@gmail.com>
2019-04-03 19:27:55 +0300

    Update Itindata_model.php

--- application/models/Itindata_model.php
+++ application/models/Itindata_model.php
@@ -12,11 +12,15 @@ class Itindata_model extends CI_Model
         $this->db->where($where);
        }
         $query = $this->db->get('sections_tbl');
-        foreach ($query->result_array() as $row)
-{
-        $data[] = $row;
-}
-        return $data;
+        if ($query -> num_rows() > 0) {
+			foreach ($query->result() as $row) {
+				$data[] = $row;
+			}
+			return $data;
+		} else {
+			return null;
+		}
+       
     }
 
     public function get_carbrands($where = null)
