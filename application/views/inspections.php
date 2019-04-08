307286be1df9d10deee8427c3a8c353e7e525c33
pavlosb <pavlos.bizimis@gmail.com>
2019-04-08 12:07:24 +0300

    Update inspections.php

--- application/views/inspections.php
+++ application/views/inspections.php
@@ -2,9 +2,9 @@
     <div class="row justify-content-center">
     <div class="col-md-8 mt-5">
         <div class="row">
-            <div class="col-sm-4 px-1 py-5  text-white text-center"><a href="<?php echo base_url(); ?>client_add" class="btn btn-success btn-lg btn-block"><i class="fal fa-user-tie fa-5x mb-2"></i><h4 >Νέος<br/>Πελάτης</h4></a></div>
-            <div class="col-sm-4 px-1 py-5  text-white text-center"><a href="<?php echo base_url(); ?>vehicle_add" class="btn btn-secondary btn-lg btn-block"><i class="fal fa-car fa-5x mb-2"></i><h4 >Νέο<br/>Όχημα</h4></a></div>
-            <div class="col-sm-4 px-1 py-5  text-white text-center"><a href="<?php echo base_url(); ?>inspection_new" class="btn btn-primary btn-lg btn-block"><i class="fal fa-clipboard-check fa-5x mb-2"></i><h4 >Νέα<br/>Επιθεώρηση</h4></a></div>
+            <div class="col-sm-4 px-1 py-5  text-white text-center"><a href="<?php echo base_url(); ?>inspection/client_add" class="btn btn-success btn-lg btn-block"><i class="fal fa-user-tie fa-5x mb-2"></i><h4 >Νέος<br/>Πελάτης</h4></a></div>
+            <div class="col-sm-4 px-1 py-5  text-white text-center"><a href="<?php echo base_url(); ?>inspection/vehicle_add" class="btn btn-secondary btn-lg btn-block"><i class="fal fa-car fa-5x mb-2"></i><h4 >Νέο<br/>Όχημα</h4></a></div>
+            <div class="col-sm-4 px-1 py-5  text-white text-center"><a href="<?php echo base_url(); ?>inspection/inspection_new" class="btn btn-primary btn-lg btn-block"><i class="fal fa-clipboard-check fa-5x mb-2"></i><h4 >Νέα<br/>Επιθεώρηση</h4></a></div>
         </div>
     </div>
     </div>
