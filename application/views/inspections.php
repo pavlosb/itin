8ec98a03f1e45375d2cc4ccff08fa3987938ceed
pavlosb <pavlos.bizimis@gmail.com>
2019-04-08 11:34:41 +0300

    Update inspections.php

--- application/views/inspections.php
+++ application/views/inspections.php
@@ -2,9 +2,9 @@
     <div class="row justify-content-center">
     <div class="col-md-8">
         <div class="row">
-            <div class="col-sm-4 px-1 py-5 bg-success text-white text-center"><i class="fal fa-user-tie fa-3x"></i><h3 class="display-4">Νέος Πελάτης</h3></div>
-            <div class="col-sm-4 px-1 py-5 bg-secondary text-white text-center"><i class="fal fa-car fa-3x"></i><h3 class="display-4">Νέο Όχημα</h3></div>
-            <div class="col-sm-4 px-1 py-5 bg-primary text-white text-center"><i class="fal fa-clipboard-check fa-3x"></i><h3 class="display-4">Νέα Επιθεώρηση</h3></div>
+            <div class="col-sm-4 px-1 py-5 bg-success text-white text-center"><i class="fal fa-user-tie fa-3x"></i><h3 >Νέος Πελάτης</h3></div>
+            <div class="col-sm-4 px-1 py-5 bg-secondary text-white text-center"><i class="fal fa-car fa-3x"></i><h3 >Νέο Όχημα</h3></div>
+            <div class="col-sm-4 px-1 py-5 bg-primary text-white text-center"><i class="fal fa-clipboard-check fa-3x"></i><h3 >Νέα Επιθεώρηση</h3></div>
         </div>
     </div>
     </div>
