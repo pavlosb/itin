476517b7d656daaac9e6c81add83299ef3e5751c
pavlosb <pavlos.bizimis@gmail.com>
2019-04-08 13:14:58 +0300

    Update inspectionform.php

--- application/views/inspectionform.php
+++ application/views/inspectionform.php
@@ -4,7 +4,7 @@
 
 <div class="container">
 <div class="row justify-content-center">
-  <div class="col-md-10">
+  <div class="col-md-8">
 <!--<h1 class="display-4">Νέo Σημείο Ελέγχου</h1> -->
 <p class="lead"></p>
 </div>
