699441a2d8cd72478e3773f8184c04da326a77cf
pavlosb <pavlos.bizimis@gmail.com>
2019-04-09 14:45:05 +0300

    Update inspectionslist.php

--- application/views/inspectionslist.php
+++ application/views/inspectionslist.php
@@ -1,5 +1,30 @@
 <div class="container">
-<div class="row justify-content-center">
+    <div class="row justify-content-center">
  <?php print_r($inspections); ?>
-</div>
+        <div class="col-12">
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th scope="col" class="text-center">Αριθμός</th>
+                        <th scope="col" class="text-center">Ημερομηνία</th>
+                        <th scope="col" class="text-center">Όχημα</th>
+                        <th scope="col" class="text-center">Πελάτης</th>
+                        <th scope="col" class="text-center"></th>
+                        <th scope="col" class="text-center"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($inspections as $insp): ?>
+                    <tr>
+                    <td class="text-center"><?php echo $insp->number_inspection; ?></td>
+                    <td class="text-center"><?php echo $insp->date_inspection; ?></td>
+                    <td class="text-center"><?php echo $insp->reg_vhcl; ?> <?php echo $insp->make_vhcl; ?></td>
+                    <td class="text-center"><?php echo $insp->name_client; ?></td>
+                    <td class="text-center"><?php echo ($insp->status_inspection > 0 ? '<i class="fal fa-check text-success"></i>' : '<i class="fal fa-hourglass-half text-primary"></i>') ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
