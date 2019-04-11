7753c1b5d200887ef2e20c93f1e64482379073a5
pavlosb <pavlos.bizimis@gmail.com>
2019-04-11 12:28:24 +0300

    updates

--- application/views/testview.php
+++ application/views/testview.php
@@ -60,51 +60,20 @@ body { font-family: DejaVu Sans; }
         <span class="page-number">Page </span>
         </footer>
         <main>
-  <h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-<i class="fal fa-abacus"></i>
-<h2>Why do we use it?</h2>
-<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-
-
-<h2>Where does it come from?</h2>
-<p class="page_break">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-<h2>What is Lorem Ipsum</h2>
-<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
-<h2>Where does it come from?</h2>
-<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-<h2>Where does it come from?</h2>
-<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+ <table>
+ <tr>
+ <td>IMPERIAL AUTOMOTIVE<br />
+DEKRA PARTNER<br />
+ΛΕΩΦΟΡΟΣ ΣΥΓΓΡΟΥ<br />
+253 ΝΕΑ ΣΜΥΡΝΗ<br />
+Τ.Κ. 17122<br />
+Τηλέφωνο 2109426352<br />
+E-Mail :<br />
+savvas.tzanis@dekra.com<br />
+</td>
+</td>
+ </tr>
+ </table>
 </main>
   </body>
   </html>
