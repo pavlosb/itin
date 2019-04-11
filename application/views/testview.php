178b1b211aca46cf826e926c24f163770fcf3efa
pavlosb <pavlos.bizimis@gmail.com>
2019-04-11 11:54:01 +0300

    Update testview.php

--- application/views/testview.php
+++ application/views/testview.php
@@ -11,12 +11,51 @@
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css">
    <style>
 body { font-family: DejaVu Sans; }
+@page {
+                margin: 100px 25px;
+            }
+
+            header {
+                position: fixed;
+                top: -60px;
+                left: 0px;
+                right: 0px;
+                height: 50px;
+
+                /** Extra personal styles **/
+                background-color: #03a9f4;
+                color: white;
+                text-align: center;
+                line-height: 35px;
+            }
+
+            footer {
+                position: fixed; 
+                bottom: -60px; 
+                left: 0px; 
+                right: 0px;
+                height: 50px; 
+
+                /** Extra personal styles **/
+                background-color: #03a9f4;
+                color: white;
+                text-align: center;
+                line-height: 35px;
+            }
 </style>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/css/tempusdominus-bootstrap-4.min.css" />
    <script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
     <title>ITIN</title>
   </head>
   <body>
+  <header>
+            Our Code World
+        </header>
+
+        <footer>
+            Copyright &copy; <?php echo date("Y");?> 
+        </footer>
+        <main>
   <h2>What is Lorem Ipsum</h2>
 <p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
 <i class="fal fa-abacus"></i>
@@ -26,5 +65,42 @@ body { font-family: DejaVu Sans; }
 
 <h2>Where does it come from?</h2>
 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+<h2>What is Lorem Ipsum</h2>
+<p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
+
+<h2>Where does it come from?</h2>
+<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+
+<h2>Where does it come from?</h2>
+<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+</main>
   </body>
   </html>
