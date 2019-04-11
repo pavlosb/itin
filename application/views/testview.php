b488aa0d121ebca36f51e1052f763d7fd0fda349
pavlosb <pavlos.bizimis@gmail.com>
2019-04-11 11:50:37 +0300

    Update testview.php

--- application/views/testview.php
+++ application/views/testview.php
@@ -19,7 +19,7 @@ body { font-family: DejaVu Sans; }
   <body>
   <h2>What is Lorem Ipsum</h2>
 <p>Το νέο στιλ ankle boots που κατέλαβε τα runways για την A/W 2019 έχει την σφραγίδα των Burberry, Alexander McQueen και Tibi. Οι μεγάλοι οίκοι έδωσαν το σύνθημα και όπως είναι γνωστό, όλοι οι υπόλοιποι θα ακολουθήσουν.</p>
-
+<i class="fal fa-abacus"></i>
 <h2>Why do we use it?</h2>
 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 
