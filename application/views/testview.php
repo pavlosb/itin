a83b6e00bd3ad45f38b266368fcbd19b048d45b1
pavlosb <pavlos.bizimis@gmail.com>
2019-04-11 13:44:29 +0300

    Update testview.php

--- application/views/testview.php
+++ application/views/testview.php
@@ -44,6 +44,7 @@ body { font-family: DejaVu Sans; }
             }
            footer .page-number:after { content: counter(page); }
             .dgreen {color:#007c3f;}
+            .frcellhdr {font-size:18px; font-weight:bold;}
            .page_break_before { page-break-before: always; }
            .page_break_after { page-break-after: always; }
 </style>
@@ -79,6 +80,10 @@ savvas.tzanis@dekra.com<br />
 </table>
 
 <table  width="100%">
+<tr style="border-bottom:1px solid #000000;">
+<td width="50%" class="frcellhdr dgreen">Διαδικασία</td>
+<td width="50%" class="frcellhdr dgreen">Επιθεώρηση</td>
+</tr>
 </table>
 </main>
   </body>
