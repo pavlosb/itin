0d888c1160a32bd31d73d639a498d63d0ed977fb
pavlosb <pavlos.bizimis@gmail.com>
2019-04-11 11:09:44 +0300

    Update Pdfgenerator.php

--- application/libraries/Pdfgenerator.php
+++ application/libraries/Pdfgenerator.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once("./vendor/dompdf/dompdf/autoload.inc.php");
+require_once("./vendor/autoload.php");
 use Dompdf\Dompdf;
 
 class Pdfgenerator {
