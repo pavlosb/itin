aa44a71fa31517a6bcd645609e3a504e97a329e9
pavlosb <pavlos.bizimis@gmail.com>
2019-04-11 11:45:50 +0300

    Update testview.php

--- application/views/testview.php
+++ application/views/testview.php
@@ -10,7 +10,7 @@
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/all.min.css">
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css">
    <style>
-body { font-family: Helvetica; }
+body { font-family: DejaVu Sans; }
 </style>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/css/tempusdominus-bootstrap-4.min.css" />
    <script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
