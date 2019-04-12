17d18fcce5c49883fef35e771b29ef4f82071761
pavlosb <pavlos.bizimis@gmail.com>
2019-04-12 11:15:53 +0300

    Update testview.php

--- application/views/testview.php
+++ application/views/testview.php
@@ -168,9 +168,9 @@ IBAN:GR8701401200120002320007025</td>
                 
             <div class="newsect-<?= $x ?>" style="width:100%">
             <table width=100%>
-            <td width=25% ><img src="<?php echo base_url(); ?>assets/images/sect-<?= $x ?>.jpg" width="100" height="98"></td>
-            <td width=25% style="margin-left:1% font-size:16px;" class="text-right dgreen">Περιγραφή οχήματος:</td>
-            <td width=18% style="margin-left:1%" class="">Τύπος οχήματος:<br />Κατασκευαστής:<br />Εμπορική ονομασία:<br />Αρ. πλαισίου:<br />Ισχύς/Κυβισμός</td>
+            <td width="25%" ><img src="<?php echo base_url(); ?>assets/images/sect-<?= $x ?>.jpg" width="100" height="98"></td>
+            <td width="25%" valign="top" style="margin-left:1% font-size:20px;" class="text-right dgreen">Περιγραφή οχήματος:</td>
+            <td width="18%" valign="top" style="margin-left:1%" class="">Τύπος οχήματος:<br />Κατασκευαστής:<br />Εμπορική ονομασία:<br />Αρ. πλαισίου:<br />Ισχύς/Κυβισμός</td>
             <td></td></table>
               <div class="mainsecthd" style="width:100% diplay:block"><?= $cp['mainsectprint']; ?></div>
       <?php 
