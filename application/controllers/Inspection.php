bb95f52654402820a8970c49cada3719fd29ab7f
pavlosb <pavlos.bizimis@gmail.com>
2019-04-12 17:47:19 +0300

    Update Inspection.php

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -142,8 +142,8 @@ class Inspection extends CI_Controller {
 			$mpdf->setFooter('{PAGENO}');
 			$mpdf->WriteHTML($html);
    			$filename = $inspection->number_inspection;
-			$dir ="/assets/pdfs";//$this->mpdfgenerator->generate($html, $filename, True, 'A4', 'portrait');	
-			$mpdf->Output($filename.".pdf",\Mpdf\Output\Destination::FILE);
+			$dir ="/assets/pdfs/";//$this->mpdfgenerator->generate($html, $filename, True, 'A4', 'portrait');	
+			$mpdf->Output($dir.$filename.".pdf",\Mpdf\Output\Destination::FILE);
 	    	
 			
 			   
