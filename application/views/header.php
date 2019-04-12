8210bcc552c432361151492d39ac586e525b7185
pavlosb <pavlos.bizimis@gmail.com>
2019-04-12 12:13:13 +0300

    updates

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.png" />
     <!-- Bootstrap CSS -->
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/all.min.css">
@@ -18,7 +18,7 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#itinnavbartoggle" aria-controls="itinnavbartoggle" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">ITIN</a>
+  <a class="navbar-brand" href="#">ITIN <img src="<?php echo base_url(); ?>assets/images/itin-dekra.png" width="24" height="30" alt=""></a>
 
   <div class="collapse navbar-collapse" id="itinnavbartoggle">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
