c4b43ecab44b530797b8ead200ce8b6c8edc097c
pavlosb <pavlos.bizimis@gmail.com>
2019-04-12 11:36:20 +0300

    Update testview.php

--- application/views/testview.php
+++ application/views/testview.php
@@ -170,7 +170,7 @@ IBAN:GR8701401200120002320007025</td>
             <td width="28%" valign="top" style="margin-left:1%; font-size:18px;" class="text-right dgreen">Περιγραφή οχήματος:</td>
             <td width="20%" valign="top" style="margin-left:1%" class="small">Τύπος οχήματος:<br />Κατασκευαστής:<br />Μοντέλο:<br />Αρ. πλαισίου:<br />Ισχύς/Κυβισμός</td>
             <td class="small"><?php echo $inspection->type_vhcl; ?><br /><?php echo $inspection->make_vhcl; ?><br /><?php echo $inspection->model_vhcl; ?><br /><?php echo $inspection->vin_vhcl; ?><br /><?php echo $inspection->pow_vhcl; ?>kW / <?php echo $inspection->displ_vhcl; ?>ccm</td></table>
-              <div class="mainsecthd" style="width:100% diplay:block"><?= $cp['mainsectprint']; ?></div>
+              <div class="mainsecthd" style="width:100%; diplay:block"><?= $cp['mainsectprint']; ?></div>
       <?php 
       
       $y= 1;
@@ -184,7 +184,7 @@ $y = $y+1;
 } ?>
 
 
-<div style="width:100%<?php if($z % 2 != 0){ echo "; background: #ccc;"; } ?>" class="pointrow">
+<div style="page-break-inside:avoid; width:100%<?php if($z % 2 != 0){ echo "; background: #ccc;"; } ?>" class="pointrow">
     <div style="width:50%; display:inline-block; min-height:20px; padding:4px 0 0 0"><?= sprintf("%02d",$z) ?> <?= $cp['printtext_cp']; ?></div>
 <div class="text-center" style="width:15%; display:inline-block; min-height:20px; padding:2px 0 0 0"><?php 
 $pointscore = $inspscore[$cp['id_cp']];
