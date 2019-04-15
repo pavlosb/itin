fc8bf7ee7e1fca549413fdc7ce4c6f532760d748
pavlosb <pavlos.bizimis@gmail.com>
2019-04-15 19:04:33 +0300

    Update Inspection.php

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -142,9 +142,9 @@ class Inspection extends CI_Controller {
 			$inspections = $this->itindata_model->get_inspectionsfull(array('id_inspection' => $id));
 			$data['inspection'] = $inspections[0];
 			$inspection = $inspections[0];
-			$data['sec1score'] = 100*($inspection->s1score_inspection / 112);
-			$data['sec2score'] = 100*($inspection->s2score_inspection / 62);
-			$data['sec3score'] = 100*($inspection->s3score_inspection / 16);
+			$data['sec1score'] = round(100*($inspection->s1score_inspection / 112), -1);
+			$data['sec2score'] = round(100*($inspection->s2score_inspection / 62), -1);
+			$data['sec3score'] = round(100*($inspection->s3score_inspection / 16), -1);
 			$data['inspscore'] = $this->itindata_model->get_inspectionscore($id);
 			$data['inspectionid'] = $id;
 			$data['checkpoints'] = $this->itindata_model->get_checkpoints();
