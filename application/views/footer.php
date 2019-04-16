8edeb392765d3e76e94c715fe5681248efef47e8
pavlosb <pavlos.bizimis@gmail.com>
2019-04-16 15:02:27 +0300

    updates

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,6 @@
 
+</div>
+<footer class="footer">created by INLINE</footer>
 
 <script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/moment.min.js"></script>
--- application/views/header.php
+++ application/views/header.php
@@ -24,6 +24,7 @@
     <title>ITIN</title>
   </head>
   <body>
+  <div class="content">  
   <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#itinnavbartoggle" aria-controls="itinnavbartoggle" aria-expanded="false" aria-label="Toggle navigation">
