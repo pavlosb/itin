055a7377231284abff18b6870bb4bd1a420f00e3
pavlosb <pavlos.bizimis@gmail.com>
2019-04-16 10:35:38 +0300

    Update inspectionview.php

--- application/views/inspectionview.php
+++ application/views/inspectionview.php
@@ -7,14 +7,16 @@
             </div>
             <div class="row">
                 <div class="col-sm-12 text-center text-light bg-green pt-1 pb-1">Στοιχεία Οχήματος</div>
-                <div class="col-sm-6 col-md-3 text-secondary">Τύπος οχήματος:</div>
-                <div class="col-sm-6 col-md-3"><?php echo $inspection->type_vhcl; ?></div>
-                <div class="col-sm-6 col-md-3 text-secondary">Θύρες:</div>
-                <div class="col-sm-6 col-md-3"><?php echo $inspection->doors_vhcl; ?></div>
-                <div class="col-sm-6 col-md-3 text-secondary">Κατασκευαστής:</div>
-                <div class="col-sm-6 col-md-3"><?php echo $inspection->make_vhcl; ?></div>
-                <div class="col-sm-6 col-md-3 text-secondary"></div>
-                <div class="col-sm-6 col-md-3"><?php echo $inspection->make_vhcl; ?></div>
+                <div class="col-sm-6 col-md-3 text-secondary pt-2 pb-2">Τύπος οχήματος:</div>
+                <div class="col-sm-6 col-md-3 pt-2 pb-2"><?php echo $inspection->type_vhcl; ?></div>
+                <div class="col-sm-6 col-md-3 text-secondary pt-2 pb-2">Θύρες:</div>
+                <div class="col-sm-6 col-md-3 pt-2 pb-2"><?php echo $inspection->doors_vhcl; ?></div>
+                <div class="col-sm-6 col-md-3 text-secondary pt-2 pb-2">Κατασκευαστής:</div>
+                <div class="col-sm-6 col-md-3 pt-2 pb-2"><?php echo $inspection->make_vhcl; ?></div>
+                <div class="col-sm-6 col-md-3 text-secondary pt-2 pb-2">Χρώμα</div>
+                <div class="col-sm-6 col-md-3 pt-2 pb-2"><?php echo $inspection->colour_vhcl; ?></div>
+                <div class="col-sm-6 col-md-3 text-secondary pt-2 pb-2">Χρώμα</div>
+                <div class="col-sm-6 col-md-3 pt-2 pb-2"><?php echo $inspection->colour_vhcl; ?></div>
 
             </div>    
 
