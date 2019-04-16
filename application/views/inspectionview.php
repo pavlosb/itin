5083636b59bd6223c7abb8c901d3ae79716c537a
pavlosb <pavlos.bizimis@gmail.com>
2019-04-16 13:24:08 +0300

    updates

--- application/views/inspectionview.php
+++ application/views/inspectionview.php
@@ -36,17 +36,17 @@
         <div class="col-lg-10 p-3 bg-light">
             <div class="row">
                 <div class="col-sm-4 text-center">
-                    <canvas id="cnvgauge5" width = "240px" height="120px"></canvas>
+                    <canvas id="cnvgauge5" ></canvas>
                     <div style="width:100%" class="text-center small">Τεχνικός έλεγχος</div>
                     <div id="score4" style="width:100%" class="text-center mb-2"><?= $sec1score ?></div>
                 </div>
                 <div class="col-sm-4 text-center">   
-                    <canvas id="cnvgauge6"  width = "240px" height="120px"></canvas>
+                    <canvas id="cnvgauge6"></canvas>
                     <div style="width:100%" class="text-center small">Έλεγχος Αμαξώματος</div>
                     <div id="score5" style="width:100%" class="text-center mb-2"><?= $sec2score ?></div>
                 </div>
                 <div class="col-sm-4 text-center">    
-                    <canvas id="cnvgauge7"  width = "240px" height="120px"></canvas>
+                    <canvas id="cnvgauge7"></canvas>
                     <div style="width:100%" class="text-center small">Έλεγχος συστήματος μνήμης σφαλμάτων</div>
                     <div id="score6" style="width:100%" class="text-center mb-2"><?= $sec3score ?></div>
                 </div>
