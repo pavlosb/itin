513a6d48de0650bdd3313d4b8241ca1ba5aae19e
pavlosb <pavlos.bizimis@gmail.com>
2019-04-16 09:39:22 +0300

    Update inspectionview.php

--- application/views/inspectionview.php
+++ application/views/inspectionview.php
@@ -12,17 +12,17 @@
         </div>
         <div class="col-sm-12 p-3 bg-light">
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-sm-4 text-center">
                     <canvas id="cnvgauge5" width = "240px" height="120px"></canvas>
                     <div style="width:100%" class="text-center small">Τεχνικός έλεγχος</div>
                     <div id="score4" style="width:100%" class="text-center mb-2"><?= $sec1score ?></div>
                 </div>
-                <div class="col-sm-4">   
+                <div class="col-sm-4 text-center">   
                     <canvas id="cnvgauge6"  width = "240px" height="120px"></canvas>
                     <div style="width:100%" class="text-center small">Έλεγχος Αμαξώματος</div>
                     <div id="score5" style="width:100%" class="text-center mb-2"><?= $sec2score ?></div>
                 </div>
-                <div class="col-sm-4">    
+                <div class="col-sm-4 text-center">    
                     <canvas id="cnvgauge7"  width = "240px" height="120px"></canvas>
                     <div style="width:100%" class="text-center small">Έλεγχος συστήματος μνήμης σφαλμάτων</div>
                     <div id="score6" style="width:100%" class="text-center mb-2"><?= $sec3score ?></div>
