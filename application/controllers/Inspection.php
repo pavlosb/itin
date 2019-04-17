a132ce5c2ed498e5636c76f82190985d595ef427
pavlosb <pavlos.bizimis@gmail.com>
2019-04-17 13:05:17 +0300

    Update Inspection.php

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -365,10 +365,10 @@ redirect('inspection/inspections_list', 'refresh');
 			$data['userid'] = $user->id;
 			$data['username'] = $user->first_name." ".$user->last_name;
 			$data['clients'] = $this->itindata_model->get_clients();
-			print_r($data['clients']);
-		//	$this->load->view('header', $data);
-		//	$this->load->view('clientslist', $data);
-		//	$this->load->view('footer', $data);
+			//print_r($data['clients']);
+			$this->load->view('header', $data);
+			$this->load->view('clientslist', $data);
+			$this->load->view('footer', $data);
 
 		} else {
 			redirect('auth/login');
