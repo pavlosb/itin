eba91d5f7e84a82e76788dda56fae5d83c820a5a
pavlosb <pavlos.bizimis@gmail.com>
2019-04-18 13:52:51 +0300

    language

---
+++ application/language/greek/itin_lang.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  ITIN Lang - Greek
+*
+* Author: Pavlos Bizimis
+* 		  pavlos.bizimis@inline.gr
+*        INLINE T.C. Ltd.
+*
+* Created:  16.04.2017
+*
+* Description:  Greek language file ITIN
+*
+*/
+$lang['new_client'] = 'Νέος<br />Πελάτης';
+$lang['new_vehicle'] = 'Νέο<br />Όχημα';
+$lang['new_inspection'] = 'Νέα<br />Επιθεώρηση';
