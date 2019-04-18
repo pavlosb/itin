dfa57d952bda7dd9d249a80394c4de2ad58acbcf
pavlosb <pavlos.bizimis@gmail.com>
2019-04-18 17:03:25 +0300

    Update vehiclesform.php

--- application/views/vehiclesform.php
+++ application/views/vehiclesform.php
@@ -1,7 +1,7 @@
 <div class="container mt-5 mb-5">
 <div class="row justify-content-center">
   <div class="col-sm-10 col-lg-6">
-<h1 class="display-4">Στοιχεία Οχήματος</h1>
+<h1 class="display-4"><?= $this->lang->line('vehicle_details'); ?></h1>
 <p class="lead"></p>
 </div>
 </div>
