fec9c1c8da9db35ebc9bca73b06cda32659255bf
pavlosb <pavlos.bizimis@gmail.com>
2019-04-22 17:57:33 +0300

    Update inspectionview.php

--- application/views/inspectionview.php
+++ application/views/inspectionview.php
@@ -89,7 +89,7 @@ jQuery(document).ready(function($) {
   var clipboard = new ClipboardJS(btns);
 
   clipboard.on('success', function(e) {
-    $(e.trigger).tooltip({title:"Copied!", trigger:'manual'}).tooltip('show').delay(800).tooltip('dispose');
+    $(e.trigger).tooltip({title:"Copied!", trigger:'manual'}).tooltip('show').delay(800).tooltip('hide');
     });
 
 var score1 = <?= $sec1score ?>;
