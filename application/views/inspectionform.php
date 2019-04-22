a3e5923f0f63b055e5c4b3c66fa2687166b60b9d
pavlosb <pavlos.bizimis@gmail.com>
2019-04-22 10:37:32 +0300

    Update inspectionform.php

--- application/views/inspectionform.php
+++ application/views/inspectionform.php
@@ -22,17 +22,17 @@
       
 <div class="col-sm-4">
 <canvas id="cnvgauge5" width = "240px" height="120px"></canvas>
-<div style="width:100%" class="text-center small">Τεχνικός έλεγχος</div>
+<div style="width:100%" class="text-center small"><?= $this->lang->line('technology_check'); ?></div>
       <div id="score4" style="width:100%" class="text-center mb-2">0</div>
 </div>
 <div class="col-sm-4">   
       <canvas id="cnvgauge6"  width = "240px" height="120px"></canvas>
-      <div style="width:100%" class="text-center small">Έλεγχος Αμαξώματος</div>
+      <div style="width:100%" class="text-center small"><?= $this->lang->line('bodywork_check'); ?></div>
       <div id="score5" style="width:100%" class="text-center mb-2">0</div>
       </div>
 <div class="col-sm-4">    
       <canvas id="cnvgauge7"  width = "240px" height="120px"></canvas>
-      <div style="width:100%" class="text-center small">Έλεγχος συστήματος μνήμης σφαλμάτων</div>
+      <div style="width:100%" class="text-center small"><?= $this->lang->line('system_check'); ?></div>
       <div id="score6" style="width:100%" class="text-center mb-2">0</div>
       </div>
 </div>
