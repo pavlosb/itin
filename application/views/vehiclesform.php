b7488140a7d2851f3c1b51d25c8735dffa897935
pavlosb <pavlos.bizimis@gmail.com>
2019-04-24 16:53:00 +0300

    Update vehiclesform.php

--- application/views/vehiclesform.php
+++ application/views/vehiclesform.php
@@ -257,7 +257,7 @@ $.ajax({
 			if (item.EXISTS == 'exists'){
 			
         fld.value ="";
-        fld.placeholder = "Υπάρχει ήδη";
+        fld.placeholder = chkval+"- Υπάρχει ήδη";
         fld.focus();
         $('#vehicleForm').formValidation('revalidateField', chkfld);
 
