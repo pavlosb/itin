0c85e318f13b716ca39d2e40512573fc9ff3f4ab
pavlosb <pavlos.bizimis@gmail.com>
2019-05-03 15:21:44 +0300

    Update Inspection.php

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -196,23 +196,7 @@ class Inspection extends CI_Controller {
 			$mpdf->Output();
 			//$mpdf->Output($dir.$filename.".pdf",\Mpdf\Output\Destination::FILE);
 			$this->itindata_model->upd_inspection($inspection->id_inspection, array($langprefix."filename_inspection" => $filename.".pdf", "status_inspection" => 1));
-			$this->lang->load('itin',$newlang);
-			$this->session->set_userdata('site_lang', $newlang);
-			$sesdata = $this->session->userdata;
-			$data['user_lang'] = $sesdata['site_lang'];
-			$html = $this->load->view('pdfreport', $data, true);
-			$mpdf = new \Mpdf\Mpdf(['format' => 'A4']);
-			$mpdf->setFooter('{PAGENO}');
-			$mpdf->WriteHTML($html);
-			$langprefix = $newprfx;
-		 	$filename = $langprefix;
-			$filename .= $this->_stringclean($inspection->number_inspection);
-			$dir ="/home/site/wwwroot/assets/pdfs/";
-		
-	$mpdf->Output($dir.$filename.".pdf",\Mpdf\Output\Destination::FILE);
-	 $this->itindata_model->upd_inspection($inspection->id_inspection, array($langprefix."filename_inspection" => $filename.".pdf"));
-	 $this->lang->load('itin', $oldlang);
-		$this->session->set_userdata('site_lang', $oldlang);
+			
 
 //	redirect ('inspection/inspections_list', 'refresh');
 			   
