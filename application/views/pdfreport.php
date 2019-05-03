ab784bc7e8c11c4463fa5ffda9886eb6311552ca
pavlosb <pavlos.bizimis@gmail.com>
2019-05-03 16:28:24 +0300

    updates

--- application/views/pdfreport.php
+++ application/views/pdfreport.php
@@ -5,17 +5,13 @@ if (isset($user_lang) && $user_lang == "greek") {
   $langprefix ="en_";
   }
 ?>
-
-
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS 
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">-->
-   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/all.min.css">
+     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/all.min.css">
     <style>
 body {font-family:DejaVuSans;font-size:13px; line-height:14px;}
 @page {
