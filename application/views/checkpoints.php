46fa017257ca16260dffad72b687876e2223e0c2
pavlosb <pavlos.bizimis@gmail.com>
2019-05-13 10:26:33 +0300

    Update checkpoints.php

--- application/views/checkpoints.php
+++ application/views/checkpoints.php
@@ -32,3 +32,31 @@ $scp = $cp['name_section'];
 </div>
 </div>
 </div>
+<a href="#" id="back-to-top" title="Back to top"><i class="fal fa-arrow-from-bottom fa-3x"></i></a>
+
+<script>
+jQuery(document).ready(function($) {
+
+  if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+});
+    </script>
