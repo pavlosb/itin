deaac08fc2725f83ab0051a81359aa814aa4b15b
pavlosb <pavlos.bizimis@gmail.com>
2019-05-14 12:09:26 +0300

    updates

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -15,9 +15,7 @@ class Auth extends CI_Controller
 		$this->load->database();
 		$this->load->library(['ion_auth', 'form_validation']);
 		$this->load->helper(['url', 'language']);
-
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
-
 		$this->lang->load('auth');
 	}
 
