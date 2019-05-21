11c46f462f344f84a62993951a0f461d049bb290
pavlosb <pavlos.bizimis@gmail.com>
2019-05-21 15:32:17 +0300

    Update vehicleslist.php

--- application/views/vehicleslist.php
+++ application/views/vehicleslist.php
@@ -28,7 +28,7 @@
                     <td class="text-center small"><?php echo $vhcl->name_client; ?></td>
                     <td class="text-left"><a class="button btn btn-outline-primary btn-sm" href="<?=base_url() ?>inspection/vehicle_edit/<?= $vhcl->id_vhcl; ?>"><i class="fal fa-edit fa-lg"></i></a>
                     <?php if (!isset($vhcl->id_inspection)) { ?>
-                        <a class="button btn btn-outline-danger btn-sm" href="<?=base_url() ?>inspection/vehicle_delete/<?= $vhcl->id_vhcl ?>" class="confirm"><i class="fal fa-times fa-lg text-danger"></i></a>                    
+                        <a class="button btn btn-outline-danger btn-sm" href="<?=base_url() ?>inspection/vehicle_delete/<?= $vhcl->id_vhcl ?>" class="confirm"><i class="fal fa-times fa-lg"></i></a>                    
                     <?php } else { ?>
 <?php echo ($vhcl->status_inspection > 0 ? '<a href="'.base_url().'inspection/inspection_view/'.$vhcl->id_inspection.'"><i class="fal fa-clipboard-check fa-lg"></i></a>' : '<a href="'.base_url().'inspection/inspection_edit/'.$vhcl->id_inspection.'"><i class="fal fa-clipboard-check fa-lg"></i></a>') ?>
                    <?php  } ?>
