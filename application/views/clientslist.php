f7802505eacc74a0f093fcb6bc0e195df4dc1867
pavlosb <pavlos.bizimis@gmail.com>
2019-05-21 16:29:51 +0300

    Update clientslist.php

--- application/views/clientslist.php
+++ application/views/clientslist.php
@@ -7,7 +7,7 @@
 </div>
     <div class="row justify-content-center">
          <div class="col-12">
-            <table id="clientslist" class="table table-striped table-sm">
+            <table id="clientslist" class="table table-striped table-smtable-responsive-sm">
                 <thead>
                     <tr>
                         <th scope="col" class="text-center"><?= $this->lang->line('name_client'); ?></th>
@@ -29,9 +29,9 @@
                     <td class="text-center"><?php echo $cl->vatno_client; ?></td>
                     <td class="text-center"><?php echo $cl->tel_client; ?></td>
                     <td class="text-center"><?php echo $cl->email_client; ?></td>
-                    <td class="text-left"><a href="<?=base_url() ?>inspection/client_edit/<?= $cl->id_client ?>"><i class="fal fa-edit"></i></a>
+                    <td class="text-left"><a class="button btn btn-outline-primary btn-sm" href="<?=base_url() ?>inspection/client_edit/<?= $cl->id_client ?>"><i class="fal fa-edit fa-lg"></i></a>
                     <?php if (!isset($cl->id_vhcl)) { ?>
-                        <a href="<?=base_url() ?>inspection/client_delete/<?= $cl->id_client ?>" class="confirm"><i class="fal fa-times text-danger"></i></a>                    
+                        <a href="<?=base_url() ?>inspection/client_delete/<?= $cl->id_client ?>" class="button btn btn-outline-danger btn-sm confirm"><i class="fal fa-trash-alt fa-lg"></i></a>                    
                     <?php } ?>
                     </td>
                     </tr>
