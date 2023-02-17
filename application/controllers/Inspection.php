a132322d4a06a5a5a52fab494a970e66bffb81eb
pavlosb <pavlos.bizimis@gmail.com>
2023-02-17 17:19:58 +0200

    Update Inspection.php

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -209,9 +209,9 @@ class Inspection extends CI_Controller {
 				}
 			
 			$html = $this->load->view('pdfreport', $data, true);
-			//$this->load->view('pdfreport');
+			$this->load->view('pdfreport');
 			//$html .= $this->load->view('footer', $data, true);
-			
+/*			
 			$mpdf = new \Mpdf\Mpdf(['format' => 'A4']);
 			$mpdf->debug = true;
 			$mpdf->setFooter('{PAGENO}');
@@ -248,7 +248,7 @@ $this->output->set_header("Cache-Control: no-cache, must-revalidate");
 $this->output->set_header("Expires: Mon, 4 Apr 1994 04:44:44 GMT");
 $this->output->set_header("Content-type: application/json");
 echo json_encode($status) ;  
-		 
+*/		 
 		} else {
 			redirect('auth/login');
 		}
