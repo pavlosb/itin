49b25f55acc9b72c03f91219517ed9afa283108b
pavlosb <pavlos.bizimis@gmail.com>
2023-03-13 12:57:31 +0200

    Update Inspection.php

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -169,7 +169,7 @@ class Inspection extends CI_Controller {
 	
 		if ($this->ion_auth->logged_in() && $this->ion_auth->in_group('inspectors'))
 		{
-			$id = $this->input->post('id');
+			//$id = $this->input->post('id');
 			$user = $this->ion_auth->user()->row();
 			$data = $this->data;
 			$ulang = $data['user_lang'];
