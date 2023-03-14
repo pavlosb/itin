4920d675be7b2673fad0c6825309e2dd8b3bddc4
pavlosb <pavlos.bizimis@gmail.com>
2023-03-14 17:27:00 +0200

    Update Inspection.php

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -766,7 +766,7 @@ echo json_encode($status) ;
 		$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$#!@';
 		$pass = array(); //remember to declare $pass as an array
 		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-		for ($i = 0; $i <style $i++) {
+		for ($i = 0; $i < 8 $i++) {
 			$n = rand(0, $alphaLength);
 			$pass[] = $alphabet[$n];
 		}
