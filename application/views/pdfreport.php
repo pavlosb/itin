15ba5a0ec0a78f1187b16d9af81cd5485a80be69
pavlosb <pavlos.bizimis@gmail.com>
2023-03-14 14:45:48 +0200

    pdf

--- application/views/pdfreport.php
+++ application/views/pdfreport.php
@@ -13,7 +13,7 @@ if (isset($user_lang) && $user_lang == "greek") {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/all.min.css">
     <style>
-			.p_1{fill:red;}
+			
 body {font-family:DejaVuSans;font-size:13px; line-height:14px;}
 @page {
                 margin: 120px 40px 120px 80px;
