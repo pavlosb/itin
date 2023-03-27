afa0e78cda8099dc4cbc9d01345a65b6cf1f5a74
pavlosb <pavlos.bizimis@gmail.com>
2023-03-27 18:43:55 +0300

    Update pdfreport.php

--- application/views/pdfreport.php
+++ application/views/pdfreport.php
@@ -203,7 +203,7 @@ if ($x==3) {?>
 											
 										}?>
 										
-<td><img class="img-fluid" src="<?= base_url() ?>upload/<?= $value ?>"/></td>
+<td><img src="<?= base_url() ?>upload/<?= $value ?>" style="width: 40mm; height: 30mm; margin: 0;"/></td>
 
 							<?php		
 						$im = $im +1;
