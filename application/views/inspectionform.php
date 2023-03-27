9fcaa8514a7e27608b98d8ad2b85778314596bf3
pavlosb <pavlos.bizimis@gmail.com>
2023-03-27 14:14:04 +0300

    Update inspectionform.php

--- application/views/inspectionform.php
+++ application/views/inspectionform.php
@@ -56,7 +56,9 @@
 						
 							<div id="results" class="row pb-3">
 								<?php if (isset($inspimg)) {
-									print_r($inspimg);
+									foreach ($inspimg as $im): ?>
+<div class="col-md-4"><img class="img-fluid" src="<?= base_url() ?>/inspection/photoupload/<?= $im['filename_img'] ?>"/></div>
+							<?php		endforeach;
 								} ?>
 							</div>
 <div class="row pb-3">
