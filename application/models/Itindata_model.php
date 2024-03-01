8079a6b42017e7c88aa922c72520c56f5910635c
pavlosb <pavlos.bizimis@gmail.com>
2024-03-01 17:52:44 +0200

    Update Itindata_model.php

--- application/models/Itindata_model.php
+++ application/models/Itindata_model.php
@@ -207,10 +207,10 @@ $maxpos = $row->maxpos;
 
     public function get_inspectionsfull($where=null) 
     {
-      $q = "Select i.*, v.*, c.*, u.first_name, u.last_name from inspections_tbl AS I
-            LEFT JOIN vehicles_tbl AS v ON i.vehicle_inspection = v.id_vhcl
-            LEFT JOIN clients_tbl AS c ON i.client_inspection = c.id_client 
-            LEFT JOIN users AS u ON i.inspector_inspection = u.id";
+      $q = "Select is.*, v.*, c.*, u.first_name, u.last_name from inspections_tbl AS is
+            LEFT JOIN vehicles_tbl AS v ON is.vehicle_inspection = v.id_vhcl
+            LEFT JOIN clients_tbl AS c ON is.client_inspection = c.id_client 
+            LEFT JOIN users AS u ON is.inspector_inspection = u.id";
 
       if (isset($where)) 
       {
