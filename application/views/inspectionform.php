7aa73da6ff6e9ef8daf1063f20ea12e0446f21f3
pavlosb <pavlos.bizimis@gmail.com>
2024-04-01 21:15:12 +0300

    fuel

--- application/views/inspectionform.php
+++ application/views/inspectionform.php
@@ -32,7 +32,7 @@ if (isset($user_lang) && $user_lang == "greek") {
 <div class="row justify-content-center">
       <div class="col-lg-8 p-3 bg-light">
       <p class="lead"><i class="fal fa-clipboard-check"></i> <?php echo $inspection->number_inspection; ?></p>
-      <p class="lead"><i class="fal fa-car"></i> <?php echo $inspection->reg_vhcl; ?> <?php echo $inspection->make_vhcl; ?> <?php echo $inspection->model_vhcl; ?></p>
+      <p class="lead"><i class="fal fa-car"></i> <?php echo $inspection->reg_vhcl; ?> <?php echo $inspection->make_vhcl; ?> <?php echo $inspection->model_vhcl; ?> <?php echo $this->lang->line($inspection->fueltyp_vhcl); ?></p>
       <p class="lead"><i class="fal fa-user-tie"></i> <?php echo $inspection->name_client; ?></p>
       </div>
       </div>
