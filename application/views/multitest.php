be41a889aee59ea3acc7cbba5e5773e70b54e43a
pavlosb <pavlos.bizimis@gmail.com>
2024-04-18 09:52:26 +0300

    multi-test

--- application/views/multitest.php
+++ application/views/multitest.php
@@ -1,6 +1,6 @@
 <?php echo form_open_multipart("inspection/imgupload", "id='inspform'");?>
 <div class="row">
-<div class = "col-md-9"><input id="fileupload" type="file" class="custom-file-input" name="fileupload" multiple/>
+<div class = "col-md-9"><input id="fileupload" type="file" class="custom-file-input" name="fileupload[]" multiple/>
 	<label class="custom-file-label" for="customFile">Επιλογή αρχείου</label></div>
 	<div class = "col-md-3">
 	<button type="submit" class="btn btn-lg btn-primary float-right"><?= $this->lang->line('submit'); ?></button>
