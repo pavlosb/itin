cb0c944a16874488a5b89ca256ac34d04115dc8f
pavlosb <pavlos.bizimis@gmail.com>
2024-04-18 11:49:22 +0300

    multiple-image

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -926,26 +926,26 @@ echo $url;
 
 
 	
-for($i=0;$i < count($_FILES["fileupload"]['name']);$i++)  
-  {  
+//for($i=0;$i < count($_FILES["fileupload"]['name']);$i++)  
+//  {  
  /* Get the name of the uploaded file */
-$filename = str_replace(' ', '_', $_FILES["fileupload"]['name'][$i]);
+//$filename = str_replace(' ', '_', $_FILES["fileupload"]['name'][$i]);
 
 /* Choose where to save the uploaded file */
-$location = "upload/".$filename;
+//$location = "upload/".$filename;
 
 /* Save the uploaded file to the local filesystem */
-if ( move_uploaded_file($_FILES["fileupload"]['tmp_name'][$i], $location) ) { 
-	$url[] = 'https://' . $_SERVER['HTTP_HOST'] .'/upload/' . $filename;
-} 
+//if ( move_uploaded_file($_FILES["fileupload"]['tmp_name'][$i], $location) ) { 
+//	$url[] = 'https://' . $_SERVER['HTTP_HOST'] .'/upload/' . $filename;
+//} 
 //$fns[] = $filename;
 //	}
-$response['url'] = $url;
+//$response['url'] = $url;
 $response['files'] = $_FILES;
 
 echo json_encode($response);
 exit; 
- }
+// }
 }
  public function dynimg($id) {
 	$pointscore = $this->itindata_model->get_scoreforoutside($id);
