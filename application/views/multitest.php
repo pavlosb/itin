144c725b6d1637dad25b81d130a9171c7c5fc775
pavlosb <pavlos.bizimis@gmail.com>
2024-04-18 11:01:07 +0300

    Revert "multi-test"
    This reverts commit 44a74c805afcec31b0fe5a3301dc7a279e0b42aa.

--- application/views/multitest.php
+++
@@ -1,9 +0,0 @@
-<?php echo form_open("inspection/imgupload", "id='inspform'");?>
-<div class="row">
-<div class = "col-md-9"><input id="fileupload" type="file" class="custom-file-input" name="fileupload" multiple/>
-	<label class="custom-file-label" for="customFile">Επιλογή αρχείου</label></div>
-	<div class = "col-md-3">
-	<button type="submit" class="btn btn-lg btn-primary float-right"><?= $this->lang->line('submit'); ?></button>
-
-</div>
-<?php echo form_close();?>
