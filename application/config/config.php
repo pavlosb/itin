f999344c39abc41fa21d1c3538e36e3603a851c1
pavlosb <pavlos.bizimis@gmail.com>
2024-04-19 17:19:23 +0300

    host

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://itin.azurewebsites.net/';
+$config['base_url'] = 'https://itin.imperial.gr/';
 //$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 //$config['base_url'] = "http";
 //$config['base_url'] .= "://".$_SERVER['HTTP_HOST']."/";
