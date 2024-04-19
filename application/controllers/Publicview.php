6de0944bb143b7f0ff725431f34cba44847efea8
pavlosb <pavlos.bizimis@gmail.com>
2024-04-19 17:40:17 +0300

    publickview

---
+++ application/controllers/Publicview.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Publicview extends CI_Controller {
+	public function index($id)
+	{
+echo $id;
+	}
+
+	public function show($id)
+	{
+		echo "show";
+echo $id;
+	}
+
+
+}
