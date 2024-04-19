006485c6c49c5bc9f58baf77ffff312c12e6d917
pavlosb <pavlos.bizimis@gmail.com>
2024-04-19 18:41:07 +0300

    publicview

--- application/controllers/Publicview.php
+++ application/controllers/Publicview.php
@@ -1,17 +1,46 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Publicview extends CI_Controller {
+class Publicview extends Inspection {
+	public function __construct()
+	{
+			parent::__construct();
+			$this->load->model('itindata_model');
+			$this->load->helper('url_helper');
+			$this->load->helper('form');
+			$this->lang->load('itin','greek');
+			$sesdata = $this->session->userdata;
+			$this->data = array(
+			'user_lang' => $sesdata['site_lang'],
+			);
+			
+	}
 	public function index($id)
 	{
 echo $id;
 	}
 
-	public function show($id)
+	public function show($qc)
 	{
-		echo "show";
-		echo "<br />";
-echo $id;
+		$data = $this->data;
+			$user = $this->ion_auth->user()->row();
+			$data['userid'] = $user->id;
+			$data['username'] = $user->first_name." ".$user->last_name;
+			$inspections = $this->itindata_model->get_inspectionsfull(array('qrcode_inspection' => $qc));
+			$data['inspection'] = $inspections[0];
+			$data['fueltypes'] = $this->_getfueltypes();
+			$data['wheeldrives'] = $this->_getwheeldrives();
+			$inspection = $inspections[0];
+			$data['sec1score'] = $inspection->s1score_inspection;
+			$data['sec2score'] = $inspection->s2score_inspection;
+			$data['sec3score'] = $inspection->s3score_inspection;
+			$data['inspscore'] = $this->itindata_model->get_inspectionscore($id);
+			//$data['inspimg'] = $this->itindata_model->get_inspectionimages($id);
+			$data['inspectionid'] = $id;
+			$data['checkpoints'] = $this->itindata_model->get_checkpoints();
+			$this->load->view('public/header', $data);
+			$this->load->view('public/publicshow', $data);
+			$this->load->view('public/footer', $data);
 	}
 
 
---
+++ application/views/public/footer.php
@@ -0,0 +1,27 @@
+
+</div>
+<footer class="footer">
+    <div class = "container ">
+        <div class="row">
+            <div class="col-sm-6 text-xs-center pt-2 pb-2">
+            <small>IMPERIAL AUTOMOTIVE DEKRA PARTNER</small>
+            </div>
+            <div class="col-sm-6 text-xs-center text-sm-right pt-2 pb-2">
+            <small>created by INLINE</small>
+            </div>
+</div></div>
+</footer>
+
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/moment.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/gauge.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/formValidation.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/framework/bootstrap4.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootbox.all.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/clipboard.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/featherlight.js" type="text/javascript" charset="utf-8"></script>
+<script src="<?php echo base_url(); ?>assets/js/featherlight.gallery.js" type="text/javascript" charset="utf-8"></script>
+  
+</body>
+</html>
