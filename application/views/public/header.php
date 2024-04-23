6a665abc748bed9e4afe904161e296a7e219313a
pavlosb <pavlos.bizimis@gmail.com>
2024-04-23 11:57:29 +0300

    Update header.php

--- application/views/public/header.php
+++ application/views/public/header.php
@@ -4,6 +4,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+		<meta http-equiv="Pragma" content="no-cache" />
+		<meta http-equiv="Expires" content="0" />
     <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.png" />
     <!-- Bootstrap CSS -->
    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
