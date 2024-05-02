ebed25eaffb291c1380fae17a0bc9e8f00f492b0
pavlosb <pavlos.bizimis@gmail.com>
2024-05-02 14:29:35 +0300

    Update inspections.php

--- application/views/inspections.php
+++ application/views/inspections.php
@@ -1,7 +1,7 @@
 
 <div class="container mt-5">
     <div class="row justify-content-center">
-    <div class="col-lg-8 mt-5">
+    <div class="col-lg-8 mt-2 mt-md-5">
         <div class="row">
             <div class="col-sm-4 px-1 py-2  text-white text-center"><a href="<?php echo base_url(); ?>inspection/client_add" class="btn btn-success btn-lg btn-block"><i class="fal fa-user-tie fa-5x mb-2"></i><h4 ><?= $this->lang->line('home_new_client'); ?></h4></a></div>
             <div class="col-sm-4 px-1 py-2  text-white text-center"><a href="<?php echo base_url(); ?>inspection/vehicle_add" class="btn btn-secondary btn-lg btn-block"><i class="fal fa-car fa-5x mb-2"></i><h4><?= $this->lang->line('home_new_vehicle'); ?></h4></a></div>
