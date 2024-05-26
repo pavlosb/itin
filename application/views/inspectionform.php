ce144b7529ef02121aeba43247c9fb9b9e209215
pavlosb <pavlos.bizimis@gmail.com>
2024-05-26 21:06:45 +0300

    Update inspectionform.php

--- application/views/inspectionform.php
+++ application/views/inspectionform.php
@@ -320,7 +320,7 @@ document.getElementById("closecamera").style.display = "block";
   let formData = new FormData(); 
 	const fileInput = document.getElementById("fileupload");
 	const selectedFiles = fileInput.files;
-const resizedImage;
+
 	//if(fileupload.files[0].length > 0) {
 	for (let j = 0; j < selectedFiles.length; j++) {
 		
