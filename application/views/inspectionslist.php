d6292f03322aad723e0c393b1ff6f55eec4d0a07
pavlosb <pavlos.bizimis@gmail.com>
2024-05-29 16:41:36 +0300

    Update inspectionslist.php

--- application/views/inspectionslist.php
+++ application/views/inspectionslist.php
@@ -11,17 +11,17 @@ if (isset($user_lang) && $user_lang == "greek") {
             <table id="inspectlist" class="table table-striped table-sm">
                 <thead>
                     <tr>
-					<th scope="col" class="text-center"><small><?= $this->lang->line('number_inspection'); ?></small></th>
-                    <th scope="col" class="text-center"><small><?= $this->lang->line('date_inspection'); ?></small></th>
+					<th scope="col" class="text-center min-tablet-l"><small><?= $this->lang->line('number_inspection'); ?></small></th>
+                    <th scope="col" class="text-center all"><small><?= $this->lang->line('date_inspection'); ?></small></th>
 						<th scope="col" class="text-center"><small><?= $this->lang->line('make_vhcl'); ?></small></th>
 						<th scope="col" class="text-center"><small><?= $this->lang->line('model_vhcl'); ?></small></th>
                         <th scope="col" class="text-center"><small><?= $this->lang->line('reg_vhcl'); ?></small></th>
 						<th scope="col" class="text-center"><small><?= $this->lang->line('vin_vhcl'); ?></small></th>
                         <th scope="col" class="text-center"><small><?= $this->lang->line('client_vhcl'); ?></small></th>
 						<th scope="col" class="text-center"><small><?= $this->lang->line('inspector'); ?></small></th>
-                        <th scope="col" class="text-center"><i class="fal fa-file-alt"></i></th>
+                        <th scope="col" class="text-center all"><i class="fal fa-file-alt"></i></th>
                         <th scope="col" class="text-center"><i class="fal fa-tachometer-fast"></i></th>
-                        <th scope="col" class="text-center"></th>
+                        <th scope="col" class="text-center all"></th>
                     </tr>
                 </thead>
                 <tbody>
