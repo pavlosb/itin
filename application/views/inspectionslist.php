edcd6508ff75823311cea2d82ee2046bc5e17de8
pavlosb <pavlos.bizimis@gmail.com>
2024-05-29 13:01:48 +0300

    Update inspectionslist.php

--- application/views/inspectionslist.php
+++ application/views/inspectionslist.php
@@ -109,6 +109,7 @@ new DataTable('#inspectlist', {
             columnDefs: [
                 {searchable: false, orderable: false, targets: [8,9,10] },
 				{targets: 1, render: DataTable.render.date('YYYY-MM-DD', 'DD-MM-YYYY', 'el')}
+				{ visible: false, targets: [5,7] }
         ],
         lengthMenu: [ 25, 50, 100 ]
         });
