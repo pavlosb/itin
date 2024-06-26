d3d9f5959846823c77ad19ee29eb1ed785916e1a
pavlosb <pavlos.bizimis@gmail.com>
2024-06-26 16:42:40 +0300

    Create email.php

---
+++ application/config/email.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$smtpass = getenv("SMTPPASS"); 
+
+$config['protocol'] = 'smtp';
+$config['wordwrap'] = TRUE;
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['crlf'] = '\r\n';
+$config['newline'] = '\r\n';
+$config['smtp_host'] = 'imperial-dekra.gr';
+$config['smtp_user'] = 'itin-noreply@imperial-dekra.gr';
+$config['smtp_pass'] = $smtpass;
+$config['smtp_port'] = '587';
+$config['smtp_crypto'] = 'TLS';
+$config['smtp_timeout'] = '5'; 
