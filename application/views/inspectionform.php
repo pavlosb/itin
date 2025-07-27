0d51f292f8a14e232337f521c40a759ec287e38c
pavlosb <pavlos.bizimis@gmail.com>
2025-07-27 20:01:00 +0300

    multiupload correct

--- application/views/inspectionform.php
+++ application/views/inspectionform.php
@@ -485,64 +485,21 @@ document.getElementById("closecamera_"+ $idcp).style.display = "block";
 }
 
  function uploadFile() {
+
+  let spinnerdiv = document.getElementById("spinner");
+  spinnerdiv.classList.add("d-flex");
+  spinnerdiv.style.removeProperty("display");
+  $("#spinner").addClass("d-flex").show();
 	i = i+1;
   let formData = new FormData(); 
 	const fileInput = document.getElementById("fileupload");
 	const selectedFiles = fileInput.files;
-	var resizedFile;
+	
 	//if(fileupload.files[0].length > 0) {
 	for (let j = 0; j < selectedFiles.length; j++) {
 		
-		
-		var file = selectedFiles[j];
-
-    // Ensure it's an image
-    if(file.type.match(/image.*/)) {
-        console.log('An image has been loaded');
-
-        // Load the image
-        var reader = new FileReader();
-        reader.onload = function (readerEvent) {
-            var image = new Image();
-            image.onload = function (imageEvent) {
-
-                // Resize the image
-                var canvas = document.createElement('canvas'),
-                    max_size = 544,// TODO : pull max size from a site config
-                    width = image.width,
-                    height = image.height;
-                if (width > height) {
-                    if (width > max_size) {
-                        height *= max_size / width;
-                        width = max_size;
-                    }
-                } else {
-                    if (height > max_size) {
-                        width *= max_size / height;
-                        height = max_size;
-                    }
-                }
-                canvas.width = width;
-                canvas.height = height;
-                canvas.getContext('2d').drawImage(image, 0, 0, width, height);
-                var dataUrl = canvas.toDataURL('image/jpeg');
-                var resizedImage = dataURLToBlob(dataUrl);
-                $.event.trigger({
-                    type: "imageResized",
-                    blob: resizedImage,
-                    url: dataUrl
-                });
-            }
-            image.src = readerEvent.target.result;
-        }
-        reader.readAsDataURL(file);
-        alert(event.blob);
-        formData.append("file[]", event.blob);
-      }
-		
-		
-    
-
+    formData.append("file[]", selectedFiles[j])
+	
 		}
 		
   //formData.append("file", fileupload[].files);
@@ -575,6 +532,9 @@ input.setAttribute("value", url);
 document.getElementById("imagefields").appendChild(input); 
 i=i+1;
 									});
+                  let spinnerdiv = document.getElementById("spinner");
+  spinnerdiv.classList.remove("d-flex");
+  spinnerdiv.style.setProperty("display", "none");                
 								}
 });
 
