13921c518821fe8025fcfbeed6ec695ac87a2464
pavlosb <pavlos.bizimis@gmail.com>
2025-07-27 19:08:17 +0300

    image handling

--- application/models/Itindata_model.php
+++ application/models/Itindata_model.php
@@ -270,7 +270,11 @@ $maxpos = $row->maxpos;
 		if ($query -> num_rows() > 0) {
 			foreach ($query->result() as $row)
 			{
-				$inspimg[$row->id_img] = $row->filename_img;
+				if ($inspimg[$row->chkpointid_img]) {
+				$inspimg[$row->chkpointid_img][$row->id_img] = $row->filename_img;
+				} else {
+				$inspimg[0][$row->id_img] = $row->filename_img;
+				}
 			}
 			return $inspimg;
 		} else {
