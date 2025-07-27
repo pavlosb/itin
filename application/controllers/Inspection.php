f154c82c67ae39927ef12870fbe8553a9f54716c
pavlosb <pavlos.bizimis@gmail.com>
2025-07-27 19:08:17 +0300

    image handling

--- application/controllers/Inspection.php
+++ application/controllers/Inspection.php
@@ -553,7 +553,7 @@ endforeach;
 if($photos) {
 foreach ($photos as $key => $filename):
 	if ($filename!=""){
-	$imgdata[] = array('inspectionid_img' => $this->input->post('inspectionid_insres'), 'filename_img' => basename($filename), 'chkpointid_img' =>'');
+	$imgdata[] = array('inspectionid_img' => $this->input->post('inspectionid_insres'), 'filename_img' => basename($filename));
 	}
 endforeach;
 }
