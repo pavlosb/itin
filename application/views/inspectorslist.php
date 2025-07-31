c576fb9c1e188d566429500d3304b744ee125fce
pavlosb <pavlos.bizimis@gmail.com>
2025-07-31 14:59:17 +0300

    Update inspectorslist.php

--- application/views/inspectorslist.php
+++ application/views/inspectorslist.php
@@ -44,7 +44,7 @@ $(document).ready(function() {
 
 var ulang = '<?= ucfirst($user_lang) ?>';
 
-    $('#inspectorslist).DataTable({
+    $('#inspectorslist')DataTable({
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/"+ ulang +".json"
             },
